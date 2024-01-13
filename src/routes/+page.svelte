a541e430dbd899cc885b9b620ee95c5ce654498c
satoshisound <robertwilliamallen@gmail.com>
2024-01-13 14:34:04 -0400

    started work to make it so you can paste in an event and sign it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { generatePrivateKey, getPublicKey, nip19, finishEvent } from 'nostr-tools';
-	import { get, writable } from 'svelte/store';
+	import { generatePrivateKey, getPublicKey, nip19, finishEvent, type Event } from 'nostr-tools';
+	import { get, writable, type Writable } from 'svelte/store';
 	import { JsonView } from '@zerodevx/svelte-json-view';
 	import { SimplePool } from 'nostr-tools';
 
@@ -13,11 +13,26 @@
 
 	let hidePrivateKey = true;
 	let globalPrivateKey = writable('');
+	let event: Writable<Event<1>> = writable({
+		kind: 1,
+		content: 'Text Content',
+		tags: [['type', 'review', 'rating']],
+		created_at: 1702644231,
+		pubkey: '525f8a81277e28ddb6f8bab0ceba8656f07c584730e74931a2942b4ad2590939',
+		id: 'b4ee7f64b6a8f26151d27c803d3de07c7c6bd40d6a69ee65f0e47fc14c80d822',
+		sig: ''
+	});
+
+	let eventText = JSON.stringify($event);
+
+	$: {
+		event.set(JSON.parse(eventText));
+	}
 
 	let metaData: any = {};
 
-	const { subscribe, set } = writable('Enokas is the king');
-	console.log(get({ subscribe }));
+	// const { subscribe, set } = writable('Enokas is the king');
+	// console.log(get({ subscribe }));
 
 	$: {
 		if (privateKey.length > 62) {
@@ -58,6 +73,7 @@
 			window['nostr'].getPublicKey().then((res) => {
 				privateKey = '';
 				PublicKey = res;
+				$event.pubkey = res;
 			});
 		}
 	}
@@ -99,15 +115,6 @@
 	}
 
 	let signedEvent = {};
-
-	const event = {
-		kind: 1,
-		content: 'Text Content',
-		tags: [],
-		created_at: 1702644231,
-		pubkey: '525f8a81277e28ddb6f8bab0ceba8656f07c584730e74931a2942b4ad2590939',
-		id: 'b4ee7f64b6a8f26151d27c803d3de07c7c6bd40d6a69ee65f0e47fc14c80d822'
-	};
 </script>
 
 <div class="p-4 w-full">
@@ -188,16 +195,21 @@
 		</div>
 		<div class="flex flex-col gap-2 md:w-6/12 border-2 p-4">
 			<span class="text-lg font-medium">Sign Event</span>
-			<textarea class="input input-bordered h-32 p-2">{JSON.stringify(event)}</textarea>
+			<textarea bind:value={eventText} class="input input-bordered h-32 p-2"></textarea>
 			<span class="divider">or</span>
-			<button class="btn btn-primary w-full" on:click={() => getSignedEvent(event)}
+			<button class="btn btn-primary w-full" on:click={() => getSignedEvent($event, true)}
 				>SignEvent</button
 			>
 			<span class="text-lg break-all font-mono"> Result:</span>
 			<span class="mockup-code break-all overflow-hidden w-full"
 				><pre class="break-all"><JsonView json={signedEvent} /></pre></span
 			>
-			<button class="btn"> Copy </button>
+			<button
+				class="btn"
+				on:click={() => navigator.clipboard.writeText(JSON.stringify(signedEvent))}
+			>
+				Copy
+			</button>
 		</div>
 		<div class="flex flex-col gap-2 md:w-6/12 border-2 p-4">
 			<span class="text-lg font-medium">Get MetaData</span>
